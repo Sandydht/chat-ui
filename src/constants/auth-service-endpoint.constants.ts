34fdccca927d37c745780aced6e185e4759eb17a
sandy <sandydwi75@gmail.com>
2025-03-16 13:13:00 +0700

    feat: update authentication service endpoint

--- src/constants/auth-service-endpoint.constants.ts
+++ src/constants/auth-service-endpoint.constants.ts
@@ -1,5 +1,5 @@
 export const AUTH_SERVICE_ENDPOINT = {
-  REGISTER: '/chat-app/api/auth/register',
-  LOGIN: '/chat-app/api/auth/login',
-  LOGOUT: '/chat-app/api/auth/logout'
+  REGISTER: '/chat-app/api/authentication/register',
+  LOGIN: '/chat-app/api/authentication/login',
+  LOGOUT: '/chat-app/api/authentication/logout'
 };
