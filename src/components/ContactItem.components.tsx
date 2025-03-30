d398cea4f037f5a65cf3471a799c8a6a92a961a1
sandy <sandydwi75@gmail.com>
2025-03-30 23:19:13 +0700

    feat: wip implement websocket

--- src/components/ContactItem.components.tsx
+++ src/components/ContactItem.components.tsx
@@ -3,6 +3,7 @@ import PersonIcon from '../assets/images/svg/perm_identity_24px.svg';
 interface ContactItemComponentProps {
   name: string;
   description: string;
+  isShowSaveButton?: boolean;
 }
 
 const ContactItem = (props: ContactItemComponentProps) => {
@@ -13,24 +14,37 @@ const ContactItem = (props: ContactItemComponentProps) => {
         className="w-full h-auto flex items-center justify-start gap-[20px] outline-none focus:outline-none focus-within:outline-none pl-[25px] cursor-pointer hover:bg-[#EEEEEE]"
       >
         <div className="w-full h-full min-w-[50px] max-w-[50px] min-h-[50px] max-h-[50px] rounded-full overflow-hidden bg-[#EEEEEE] flex items-center justify-center">
-          <img 
+          <img
             src={PersonIcon}
             alt='Person icon'
             loading='lazy'
             className='w-full h-full min-w-[24px] max-w-[24px] min-h-[24px] max-h-[24px]'
           />
         </div>
-        <div className='w-full h-auto pr-[25px] py-[10px] border-b-[1px] border-[#D9D9D9] flex flex-col items-start justify-start'>
-          <div className='w-full h-auto'>
-            <p className='text-left text-[16px] leading-[24px] text-[#000000] font-medium'>
-              {props.name}
-            </p>
+        <div className='w-full h-auto flex items-center justify-center gap-[10px] border-b-[1px] border-[#D9D9D9] pr-[25px] py-[10px]'>
+          <div className='w-full h-auto flex flex-col items-start justify-start'>
+            <div className='w-full h-auto'>
+              <p className='text-left text-[16px] leading-[24px] text-[#000000] font-medium'>
+                {props.name}
+              </p>
+            </div>
+            <div className='w-full h-auto'>
+              <p className='text-left text-[14px] leading-[20px] text-[#8696A0] font-medium'>
+                {props.description}
+              </p>
+            </div>
           </div>
-          <div className='w-full h-auto'>
-            <p className='text-left text-[14px] leading-[20px] text-[#8696A0] font-medium'>
-              {props.description}
-            </p>
-          </div>
+          
+          {props.isShowSaveButton && (
+            <>
+              <button
+                type='button'
+                className='w-auto h-auto px-[10px] py-[5px] rounded-[6px] cursor-pointer bg-[#508C9B]'
+              >
+                <p className='text-center text-[14px] leading-[20px] text-[#FFFFFF] font-medium'>Simpan</p>
+              </button>
+            </>
+          )}
         </div>
       </button>
     </>
